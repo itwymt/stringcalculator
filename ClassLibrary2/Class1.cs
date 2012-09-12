917c5ff4ae7f3c54816d48899d32edbb688cd627
unknown <heleng@mak007.compatibl.com>
2012-09-12 15:31:59 +0300

    string calculator

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+
+namespace ClassLibrary2
+{
+    public class TestStringCalculator
+    {
+        [Fact]
+        public void should_return_0_for_empty_string()
+        {
+            //aggreggate
+            //var calculator = new StringCalculator();
+
+            //act 
+            var result = StringCalculator.Add("");
+
+            //assert
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void should_return_result_for_one_number()
+        {
+            //aggreggate 
+            //var calculator = new StringCalculator();
+
+            //act 
+            var result = StringCalculator.Add("1");
+
+            //assert
+            result.Should().Be(1);
+        }
+
+        [Fact]
+        public void should_handle_miltiple_numbers()
+        {
+            //assign
+            //var calculator = new StringCalculator();
+
+            //act
+            var result = StringCalculator.Add("1,2,3");
+
+            //assert
+            result.Should().Be(6);
+        }
+
+        [Fact]
+        public void should_handle_new_line_delimiter()
+        {
+            //assign
+            //var calculator = new StringCalculator();
+
+            //act
+            var result = StringCalculator.Add("1\n2,3,4");
+
+            //assert
+            result.Should().Be(10);
+        }
+
+        [Fact]
+        public void should_handle_specified_delimiter()
+        {
+            //assign
+            //var calculator = new StringCalculator();
+
+            //act
+            var result = StringCalculator.Add("//##\n1,2,3##10##12");
+
+            //assert
+            result.Should().Be(28);
+        }
+
+        [Fact]
+        public void should_report_error_for_neagtives()
+        {
+            //assign
+            //var calculator = new StringCalculator();
+
+            //act
+            Action action = () => StringCalculator.Add("//@@\n-1@@3@@-3@@-4,7");
+
+            //assert
+            action.ShouldThrow<Exception>().WithMessage("Negatives are not allowed. Negative values: -1, -3, -4");
+        }
+
+        [Fact]
+        public void souldnot_support_numbers_bigger_then_1000()
+        {
+            //assign
+            //var calculator = new StringCalculator();
+
+            //act
+            var result = StringCalculator.Add("//qq\n1002,2000,3qq2");
+
+            //assert
+            result.Should().Be(5);
+        }
+
+        [Fact]
+        public void should_support_multiple_delimiters()
+        {
+            //assign
+            //var calculator = new StringCalculator();
+
+            //act
+            var result = StringCalculator.Add("//[%][$$][^^D]\n1$$5^^D4,1,1002,3%1");
+
+            //assert
+            result.Should().Be(15);
+        }
+    }
+}
---
+++ ClassLibrary2/StringCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary2
+{
+    class StringCalculator
+    {
+        internal static object Add(string p)
+        {
+            var delimiters = new List<string> {",","\n"};
+
+            if (p.StartsWith("//"))
+            {
+                var indexOfN = p.IndexOf('\n');
+                delimiters.AddRange(p.Substring(2, indexOfN - 2).Split(new []{'[',']'}));
+                p = p.Substring(indexOfN, p.Length - indexOfN);
+            }
+
+            var digits = p.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries).Where(s => Int32.Parse(s) <= 1000).ToList();
+
+            var negatives = digits.Where(s => Int32.Parse(s) < 0).ToList();
+            if (negatives.Any())
+            {
+                throw new Exception(String.Format("Negatives are not allowed. Negative values: {0}", String.Join(", ", negatives)));
+            }
+
+            return digits.Sum(s => Int32.Parse(s));
+        }
+    }
+}
